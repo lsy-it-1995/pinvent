08b2bcf8dbf36fee0cdfa013df25ea46d8be4124
lsy <garyshaoyu@gmail.com>
2024-01-06 15:32:12 -0600

    @55:53 finish Create Folders and Home Route

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,17 @@ const cors = require("cors");
 // const cookieParser = require("cookie-parser");
 // const path = require("path");
 
-const app = express()
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json())
+
+
+app.get("/", (req, res) => {
+    res.send("Home page");
+})
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
